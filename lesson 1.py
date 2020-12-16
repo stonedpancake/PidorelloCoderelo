367b6771cfdd94c6ebd1c829dac45c278d2c3623
stonedpancake <slavaqwe>
2020-12-16 13:44:09 +0300

    1

---
+++ lesson 1.py
@@ -0,0 +1,43 @@
+# helou mr. Pidorello!
+
+# this is your first lesson
+# u can start program by click to the RUN button
+# '#' this sharp means comment
+# everything u write after sharp wouldn't compile
+
+# the first thing u should know is IMMORTAL PRINT print() это функция, которая выводит в консоль то, что ты передашь
+# ей в скобочках (это называется аргумет (всё, что находтся в скобочках - это аргументы (если их нет, скобочки должны
+# быть пустыми (), но они должны быть!))) ниже по реке, твоему взору представилось великолепие мира кодерского - кот.
+
+print('    *    ' + '\t' + '    *    ')
+print('   * *   ' + '\t' + '   * *   ')
+print('  *   *  ' + '\t' + '  *   *  ')
+print(' ###################')
+print(' #                 #')
+print(' #                 #')
+print(' #   \        /    #')
+print(' #       ==        #')
+print(' #                 #')
+print(' ###################')
+
+# кот создан из кода (BA-DUM-TSS)
+# пока котик сердится
+# поднять котику настроение можешь толко ты!
+# это и есть твоё первое задание
+
+# Подними настроение котику
+
+
+# Ну как? Удалось? Тогда поехали дальше (ут-ту): напиши снизу что такое '\t', который я исползовал выше?
+
+'''
+Jingle bells, jingle bells
+Jingle all the way
+Oh, what fun it is to ride
+In a one horse open sleigh
+
+Hey, jingle bells, jingle bells
+Jingle all the way
+Oh, what fun it is to ride
+In a one horse open sleigh
+'''
---
+++ lesson 2.py
@@ -0,0 +1,34 @@
+"""
+Jingle bells, jingle bells
+Jingle all the way
+Oh, what fun it is to ride
+In a one horse open sleigh
+
+Hey, jingle bells, jingle bells
+Jingle all the way
+Oh, what fun it is to ride
+In a one horse open sleigh
+"""
+
+# helou mr. Pidorello!
+# this is your second lesson
+
+
+"""трое ковычек с каждой стороны ('''какой-нибодь текст''') - тоже комментарии, только в этом случае комментарием
+будет не строка, как в случае с #, а всё, что ты напишешь между ковычками """
+
+'''почему я использовал три двойные ковычки, хотя перед этим использовал одинарные? Дело в том, что если в одних 
+ковычках есть другие, оди должны быть другими, т.е. если. в одинарных ковычках ты хочешь использовать ковычки, 
+ты должен использовать двойные и наоборот. '''
+
+'''Ниже есть код: сделай так, чтобы он выполнялся, напиши что такое input() и исправь в нём ощибку'''
+
+# Код:
+
+'''
+
+print('Whats your name, Pidorello?')
+pidorellos_name = input
+print('Hello, Pidorello! Your name is Pidorello ' + pidorellos_name)
+
+'''
